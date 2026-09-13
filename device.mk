40357c6cad65ed633e60534c79c5cfb855aad11c
Yumi Yukimura <me.cafebabe@gmail.com>
2026-09-13 19:26:02 +0800

    ciri: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- device.mk
+++ device.mk
@@ -403,7 +403,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti \
+    oplus_usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
